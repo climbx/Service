c1fef5c1f30c92ab6a96d0a98f8730bf510c6c30
Pierre Gaimard <pierre@gaimard-family.fr>
2021-08-05 13:14:10 +0200

    Add missing Test Service class

---
+++ src/Tests/TestServiceWithNoConstructorArgument.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Climbx\Service\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @codeCoverageIgnore
+ */
+class TestServiceWithNoConstructorArgument
+{
+    public function __construct()
+    {
+    }
+}
